57f7ea726b4d200f6e2781dfb4d91cb6fca2a9f7
martinhadid <martinhadid@gmail.com>
2020-03-13 17:50:48 +0200

    Updates

--- precache-manifest.eeb6b68255ff2fee68edd63c806fa5b4.js
+++ precache-manifest.eeb6b68255ff2fee68edd63c806fa5b4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef10961676d9f1da4804a05dc7d9561f",
+    "revision": "7f806748a9dccdae217f80b2881928a6",
     "url": "/martinhadid/index.html"
   },
   {
-    "revision": "c709c7e837e9d957c607",
+    "revision": "2c847d2bf14f9e999647",
     "url": "/martinhadid/static/css/main.5facb584.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/martinhadid/static/js/2.436f8c2b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c709c7e837e9d957c607",
-    "url": "/martinhadid/static/js/main.cdc877c1.chunk.js"
+    "revision": "2c847d2bf14f9e999647",
+    "url": "/martinhadid/static/js/main.06140690.chunk.js"
   },
   {
     "revision": "7f954289a41e5aee4f06",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/martinhadid/precache-manifest.dae92b3fdeb1236fda2462ebec24af67.js"
+  "/martinhadid/precache-manifest.eeb6b68255ff2fee68edd63c806fa5b4.js"
 );
 
 self.addEventListener('message', (event) => {
