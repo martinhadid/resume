03d00117a0ee5b9f6dfa769f5f3d92aeb1c83854
martinhadid <martinhadid@gmail.com>
2020-03-18 17:05:36 +0200

    Updates

--- precache-manifest.63aa7120dbd77f01b6bac09279b3ae40.js
+++ precache-manifest.63aa7120dbd77f01b6bac09279b3ae40.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9116261c7ebf2441538e957518591c8f",
+    "revision": "725c884d0ec7d044fbb7781afb29061b",
     "url": "/martinhadid/index.html"
   },
   {
-    "revision": "f93a06215bf2f86579fa",
+    "revision": "a80bec82c7c52cbaaf78",
     "url": "/martinhadid/static/css/main.5facb584.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/martinhadid/static/js/2.436f8c2b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f93a06215bf2f86579fa",
-    "url": "/martinhadid/static/js/main.948dbd87.chunk.js"
+    "revision": "a80bec82c7c52cbaaf78",
+    "url": "/martinhadid/static/js/main.636adffe.chunk.js"
   },
   {
     "revision": "7f954289a41e5aee4f06",
