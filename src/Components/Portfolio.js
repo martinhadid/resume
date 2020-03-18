34667b9198376cc7a961b08597c88d04f293f418
martinhadid <martinhadid@gmail.com>
2020-03-18 18:38:16 +0200

    remove .

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -27,7 +27,7 @@ class Portfolio extends Component {
       <section id="portfolio">
       <div className="row">
          <div className="twelve columns collapsed">
-            <h1>Check Out Some of My Projects.</h1>
+            <h1>Check Out Some of My Projects</h1>
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
             </div>
